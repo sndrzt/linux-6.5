4f5cdb3322106bf1c1fbe2df0f4da32848e366e8
Sndrz Teo <sndrz@outlook.com>
2013-04-10 16:42:39 +0800

    remove unused files
    Signed-off-by: Sndrz Teo <sndrz@outlook.com>

--- build.sh
+++ build.sh
@@ -13,3 +13,29 @@ date > b.txt
 
 sudo make modules_install install
 
+#./arch/x86/crypto/sha1-ssse3.ko
+#./arch/x86/crypto/sha256-ssse3.ko
+#./sound/hda/snd-intel-sdw-acpi.ko
+#./sound/soc/snd-soc-acpi.ko
+#./drivers/soundwire/soundwire-bus.ko
+#./drivers/soundwire/soundwire-cadence.ko
+#./drivers/soundwire/soundwire-generic-allocation.ko
+#./drivers/soundwire/soundwire-intel.ko
+#./sound/core/snd-pcm-dmaengine.ko
+#./sound/core/snd-rawmidi.ko
+#./sound/core/seq/snd-seq-midi.ko
+#./sound/core/seq/snd-seq-midi-event.ko
+#./sound/soc/intel/common/snd-soc-acpi-intel-match.ko
+#./sound/soc/codecs/snd-soc-hdac-hda.ko
+#./sound/soc/intel/skylake/snd-soc-skl.ko
+#./sound/soc/intel/common/snd-soc-sst-dsp.ko
+#./sound/soc/intel/common/snd-soc-sst-ipc.ko
+#./sound/soc/sof/snd-sof.ko
+#./sound/soc/sof/intel/snd-sof-intel-hda.ko
+#./sound/soc/sof/intel/snd-sof-intel-hda-common.ko
+#./sound/soc/sof/intel/snd-sof-intel-hda-mlink.ko
+#./sound/soc/sof/snd-sof-pci.ko
+#./sound/soc/sof/intel/snd-sof-pci-intel-apl.ko
+#./sound/soc/sof/snd-sof-utils.ko
+#./sound/soc/sof/xtensa/snd-sof-xtensa-dsp.ko
+
